4eadad142d7475e3ad5f25888b97a4c8a39559dd
alessandrodigiacomantonio <alessandrodigiacomantonio94@gmail.com>
2020-01-18 16:25:56 +0100

    Implementata funzionalità "prev" allo slider

--- js/script.js
+++ js/script.js
@@ -11,3 +11,16 @@ $('.next').on('click', function() {
     $('.images .active').first().toggleClass('active');
   }
 });
+$('.prev').on('click', function() {
+  if ($('.nav .active').hasClass('first')) {
+    $('.nav .first').toggleClass('active');
+    $('.nav .last').toggleClass('active');
+    $('.images .first').toggleClass('active');
+    $('.images .last').toggleClass('active');
+  } else {
+    $('.nav .active').prev().toggleClass('active');
+    $('.nav .active').last().toggleClass('active');
+    $('.images .active').prev().toggleClass('active');
+    $('.images .active').last().toggleClass('active');
+  }
+});
