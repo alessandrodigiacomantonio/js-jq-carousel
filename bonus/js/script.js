c752beec497772e5d0c1f608975deffccead6d4f
alessandrodigiacomantonio <alessandrodigiacomantonio94@gmail.com>
2020-01-18 16:36:59 +0100

    bonus: commit iniziale

---
+++ bonus/js/script.js
@@ -0,0 +1,29 @@
+// ↓ script per visualizzare l'immagine successiva dello slider ↓
+$('.next').on('click', function() {
+  if ($('.nav .active').hasClass('last')) {
+    $('.nav .last').toggleClass('active');
+    $('.nav .first').toggleClass('active');
+    $('.images .last').toggleClass('active');
+    $('.images .first').toggleClass('active');
+  } else {
+    $('.nav .active').next().toggleClass('active');
+    $('.nav .active').first().toggleClass('active');
+    $('.images .active').next().toggleClass('active');
+    $('.images .active').first().toggleClass('active');
+  }
+});
+
+// ↓ script per visualizzare l'immagine precedente dello slider ↓
+$('.prev').on('click', function() {
+  if ($('.nav .active').hasClass('first')) {
+    $('.nav .first').toggleClass('active');
+    $('.nav .last').toggleClass('active');
+    $('.images .first').toggleClass('active');
+    $('.images .last').toggleClass('active');
+  } else {
+    $('.nav .active').prev().toggleClass('active');
+    $('.nav .active').last().toggleClass('active');
+    $('.images .active').prev().toggleClass('active');
+    $('.images .active').last().toggleClass('active');
+  }
+});
