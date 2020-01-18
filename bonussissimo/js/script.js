bdf30fe6281b564304d428e4e56a5ee21cc9d670
alessandrodigiacomantonio <alessandrodigiacomantonio94@gmail.com>
2020-01-18 19:08:11 +0100

    Bonussissimo: scelta immagini possibile dai cerchi nella navbar

--- bonussissimo/js/script.js
+++ bonussissimo/js/script.js
@@ -1,23 +1,36 @@
 $(document).ready(function() {
-  // ↓ onClick events ↓
+
+  //------ ↓ onClick events ↓ ------
+
+  // ↓ slider: freccia destra ↓
   $('.next').on('click', function() {
     activeNext();
   });
+
+  // ↓ slider: freccia sinistra ↓
   $('.prev').on('click', function() {
     activePrev();
   });
 
-  // ↓ keybindings ↓
-  $('html').on('keydown', function(event) {
-    if (event.which == 39) activeNext();
-    if (event.which == 37) activePrev();
+  // ↓ slider: cerchi nella navbar ↓
+  $('.nav > *').on('click', function() {
+    activeSelectedCircle($(this));
   });
+
+  //------ ↑ onClick events ↑ ------
+
+  //------ ↓ scorciatoie da tastiera ↓------
+  $('html').on('keydown', function(event) {
+    if (event.which == 39) activeNextImg();
+    if (event.which == 37) activePrevImg();
+  });
+  //------ ↑ scorciatoie da tastiera ↑------
 });
 
-//----- ↓ funzioni ↓ ----
+//------ ↓ funzioni ↓ ------
 
-// ↓ script per visualizzare l'immagine successiva dello slider ↓
-function activeNext() {
+// ↓ funzione per visualizzare l'immagine successiva dello slider ↓
+function activeNextImg() {
   if ($('.nav .active').hasClass('last')) {
     $('.nav .last').toggleClass('active');
     $('.nav .first').toggleClass('active');
@@ -30,8 +43,8 @@ function activeNext() {
     $('.images .active').first().toggleClass('active');
   }
 }
-// ↓ script per visualizzare l'immagine precedente dello slider ↓
-function activePrev() {
+// ↓ funzione per visualizzare l'immagine precedente dello slider ↓
+function activePrevImg() {
   if ($('.nav .active').hasClass('first')) {
     $('.nav .first').toggleClass('active');
     $('.nav .last').toggleClass('active');
@@ -44,3 +57,20 @@ function activePrev() {
     $('.images .active').last().toggleClass('active');
   }
 }
+
+// ↓ funzione per visualizzare qualsiasi immagine nello slider cliccando sui cerchi della navbar nello slider ↓
+function activeSelectedCircle(circleClicked) {
+  if (! circleClicked.hasClass('active')) {
+    $('.nav > *').removeClass('active');
+    circleClicked.addClass('active');
+    var i = 0;
+    var index = 0;
+    while (i < $('.nav > *').length) {
+      if ($('.nav > *:nth-of-type('+(i+1)+')').hasClass('active')) index = i+1;
+      i++;
+    }
+    $('.images > *').removeClass('active');
+    $('.images > *:nth-of-type('+index+')').addClass('active');
+  }
+}
+//------ ↑ funzioni ↑ ------
